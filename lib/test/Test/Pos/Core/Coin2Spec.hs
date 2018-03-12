032fd3077c1d1db1120dcd9451c4a44072f5da86
John Hughes <john.hughes@quviq.com>
2018-03-12 17:45:38 +0100

    Added a tiny, simplified version of Coin2Spec.

---
+++ lib/test/Test/Pos/Core/Coin2Spec.hs
@@ -0,0 +1,40 @@
+-- | Simpler specification of Pos.Core.Coin
+
+module Test.Pos.Core.Coin2Spec ( spec ) where
+
+import Universum
+
+import Test.QuickCheck
+import Test.Hspec
+import Test.Hspec.QuickCheck
+
+import Pos.Core.Common
+--import Pos.Arbitrary.Core
+
+import Control.Exception (ErrorCall)
+
+-- A generator for Coin values that tries to select interesting cases
+
+newtype C = C Coin deriving Show
+
+instance Arbitrary C where
+  arbitrary = (C . mkCoin) <$>
+  	      (oneof $ oneof [choose (0,10),
+  	      		      ((-) maxCoinVal) <$> choose (0,10)] :
+		       [choose(0,n) | n <- takeWhile (>10) $
+	          		      	     iterate (`div` 1000) maxCoinVal])
+
+--  shrink (C coin) = map C (shrink coin)
+
+spec = describe "Coin properties" $ do
+    prop "unsafeAddCoin" ((+) `models` unsafeAddCoin)
+    prop "unsafeSubCoin" ((-) `models` unsafeSubCoin)
+--    prop "unsafeMulCoin" ((*) `models` unsafeMulCoin)
+
+(op `models` impl) (C c1) (C c2) =
+    cover ok       40 "successful case" .
+    cover (not ok)  5 "exceptional case" $
+    ioProperty (catch (return $! unsafeGetCoin (c1 `impl` c2) == ans)
+                      (\(_ :: ErrorCall) -> return $ not ok))
+    where ans = unsafeGetCoin c1 `op` unsafeGetCoin c2
+    	  ok  = ans >= 0 && ans <= maxCoinVal
